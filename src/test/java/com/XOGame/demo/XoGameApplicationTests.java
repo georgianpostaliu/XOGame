db10cbf7c76dd9ed6810b75a486bdce451d2802a
Georgian Postaliu <georgian.postaliu@gmail.com>
2023-09-07 12:27:58 +0300

    Initial commit

---
+++ src/test/java/com/XOGame/demo/XoGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XOGame.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XoGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
